Units bug fix: sim.pixel_trans and sim.pixel_tot. More robust Gaussian fit to compute RVs

SOAP/SOAP.py
@@ -893,7 +893,7 @@ class Simulation:
                 pixel_tot_np = np.tile(pixel_quiet, (psi.size, 1)) - out_np[3]
             else:
                 pixel_tot_np=np.tile(pixel_quiet, (psi.size, 1))
-            self.pixel_shadow=((pixel_tot_np-save_pixel_tot).T/np.max(pixel_quiet)).T
+            self.pixel_shadow=((pixel_tot_np-save_pixel_tot).T/np.max(save_pixel_tot)).T
             
 
 
@@ -984,7 +984,7 @@ class Simulation:
                 plt.plot(_rv, ccf_pixel_flux.T)
                 plt.show()
 
-            _ = self.get_results(_rv, ccf_pixel_flux, skip_fwhm, skip_bis)
+            _ = self.get_results(_rv, -ccf_pixel_flux, skip_fwhm, skip_bis)
             rv_flux, fwhm_flux, span_flux = _
             if template:
                 ccf_pixel_bconv = np.array(
@@ -1005,7 +1005,7 @@ class Simulation:
                     ]
                 )
 
-            _ = self.get_results(_rv, ccf_pixel_bconv, skip_fwhm, skip_bis)
+            _ = self.get_results(_rv, -ccf_pixel_bconv, skip_fwhm, skip_bis)
             rv_bconv, fwhm_bconv, span_bconv = _
             if template:
                 ccf_pixel_tot = np.array(
@@ -1027,7 +1027,7 @@ class Simulation:
                 )
             self.ccf=ccf_pixel_tot
             self.rv=_rv
-            _ = self.get_results(_rv, ccf_pixel_tot, skip_fwhm, skip_bis)
+            _ = self.get_results(_rv, -ccf_pixel_tot, skip_fwhm, skip_bis)
             rv_tot, fwhm_tot, span_tot = _
             if DEBUG:
                 plt.plot(rv_tot)
@@ -1100,9 +1100,7 @@ class Simulation:
 
                 slope_rvs = slope_coefs[0] * planet_phases + slope_coefs[1]
                 slope_flux= slope_flux_coefs[0] * planet_phases + slope_flux_coefs[1]
-                #import matplotlib.pyplot as plt
-                # plt.plot(FLUXstar/slope_flux)
-                # plt.show()
+
                 if DEBUG == True:
                     print("Slopes coefficients out-of-transit")
                     print(slope_coefs)
@@ -1115,7 +1113,7 @@ class Simulation:
 
                     corr_pixel_flux = np.array(
                         [
-                            stspnumba.doppler_shift(pixel.wave, pixel_tot[i], -slope_rvs[i])
+                            stspnumba.doppler_shift(pixel.wave, pixel_tot[i], -slope_rvs[i]*1000)
                             for i in range(len(pixel_tot))
                         ]
                     )
@@ -1133,6 +1131,7 @@ class Simulation:
                         for j in range(len(corr_pixel_flux))
                     ]
                 )
+
                 self.master_out_fw = np.mean(flux_weighted_spectra[phase_mask], axis=0)
                 self.integrated_spectra_fw = flux_weighted_spectra
 
@@ -1163,7 +1162,7 @@ class Simulation:
                 # shift by the Keplerian RV
                 for i in range(pixel_tot.shape[0]):
                     pixel_tot[i] = stspnumba.doppler_shift(
-                        pixel.wave, pixel_tot[i], rv_kep[i].value
+                        pixel.wave, pixel_tot[i], rv_kep[i].value*1000
                     )
             else:
                 # shift by the Keplerian RV

SOAP/gaussian.py
@@ -231,64 +231,6 @@ def levenberg_marquardt(a,b,rv, ccf, p0):
         jac=lambda p: jacobian_gaussian(rv, p),
     )
     return res.x
-# def levenberg_marquardt(
-#     func, jacobian, x_data, y_data, theta_init, max_iter=200, tol=1e-8, lambda_init=0.01
-# ):
-#     """
-#     Levenberg-Marquardt algorithm for non-linear least squares optimization.
-
-#     Parameters:
-#     - func: A function that returns the model prediction for given parameters.
-#     - jacobian: A function that returns the Jacobian matrix of the model.
-#     - x_data: Input data for the model.
-#     - y_data: Observed data.
-#     - theta_init: Initial guess for the parameters [A, mu, sigma, C].
-#     - max_iter: Maximum number of iterations.
-#     - tol: Tolerance for convergence.
-#     - lambda_init: Initial damping factor.
-
-#     Returns:
-#     - theta: Optimized parameters [A, mu, sigma, C].
-#     """
-#     # Initial parameter guess
-#     theta = np.array(theta_init, dtype=float)
-#     lambda_ = lambda_init  # Initial damping factor
-
-#     for iteration in range(max_iter):
-#         # Compute residuals and Jacobian at the current parameters
-#         residuals = func(x_data, theta) - y_data
-#         jacobian_matrix = jacobian(x_data, theta)
-
-#         # Compute the normal equation: (J^T * J + lambda * I) * delta_theta = J^T * residuals
-#         JTJ = np.dot(jacobian_matrix.T, jacobian_matrix)  # J^T * J
-#         JTr = np.dot(jacobian_matrix.T, residuals)  # J^T * residuals
-
-#         # Identity matrix of the same size as JTJ
-#         I = np.eye(len(theta))
-
-#         # Update step (delta_theta)
-#         A = JTJ + lambda_ * I
-#         delta_theta = np.linalg.solve(A, JTr)
-
-#         # Update the parameters
-#         theta_new = theta - delta_theta
-
-#         # Compute new residuals and check for convergence
-#         residuals_new = func(x_data, theta_new) - y_data
-#         if np.linalg.norm(residuals_new) < np.linalg.norm(residuals):
-#             # If residuals decreased, accept the new parameters and reduce lambda
-#             lambda_ *= 0.1
-#             theta = theta_new
-#         else:
-#             # If residuals did not decrease, increase lambda
-#             lambda_ *= 10
-
-#         # Check for convergence (change in parameters is small enough)
-#         if np.linalg.norm(delta_theta) < tol:
-#             # print(f"Converged in {iteration} iterations.")
-#             break
-
-#     return theta
 
 
 def compute_rv_fwhm(rv: np.ndarray, ccf: np.ndarray):
@@ -300,11 +242,6 @@ def compute_rv_fwhm(rv: np.ndarray, ccf: np.ndarray):
         ccf,
         np.array([abs(np.max(ccf) - np.min(ccf)), rv[np.argmin(ccf)], 3, np.max(ccf)]),
     )
-    # import matplotlib.pyplot as plt
-    # plt.plot(rv,ccf,".k")
-    # plt.plot(rv,gaussian(rv,res),".r")
-    # plt.plot(rv,gaussian(rv,np.array([np.min(ccf)-np.max(ccf),0, 3,np.max(ccf)])),".g")
-    # plt.show()
     return res[1], 2 * np.sqrt(2 * np.log(2)) * res[2]
 
 
@@ -317,12 +254,12 @@ def compute_rv(rv: np.ndarray, ccf: np.ndarray):
         ccf,
         np.array([abs(np.max(ccf) - np.min(ccf)), rv[np.argmin(ccf)], 3, np.max(ccf)]),
     )
-
     return res[1]
 
 
 def compute_rv_fwhm_bis(rv: np.ndarray, ccf: np.ndarray):
     """Fit a Gaussian to rv, ccf and return the estimated RV and FWHM"""
+
     res = levenberg_marquardt(
         gaussian,
         jacobian_gaussian,
@@ -330,10 +267,5 @@ def compute_rv_fwhm_bis(rv: np.ndarray, ccf: np.ndarray):
         ccf,
         np.array([np.min(ccf) - np.max(ccf), rv[np.argmin(ccf)], 3, np.max(ccf)]),
     )
-    # import matplotlib.pyplot as plt
-    # plt.plot(rv,ccf,".k")
-    # plt.plot(rv,gaussian(rv,res),".r")
-    # plt.plot(rv,gaussian(rv,np.array([np.min(ccf)-np.max(ccf),0, 3,np.max(ccf)])),".g")
-    # plt.show()
     bis = BIS_HARPS(rv, ccf, res)
     return res[1], 2 * np.sqrt(2 * np.log(2)) * res[2], -bis

SOAP/visualizer.py
@@ -42,7 +42,7 @@ def visualize(sim, out, plot_type, lim=None, ref_wave=None, plot_lims=None, show
         psi_i = out.psi
 
     # Grid resolution, geometry, and arrays (match animate)
-    grid_res = 0.0005
+    grid_res = 0.003
     Vsini = (2 * np.pi * sim.star.radius.value * 696340) / (sim.star.prot.value * 24 * 3600)
     i = np.radians(sim.star.incl.value)
 